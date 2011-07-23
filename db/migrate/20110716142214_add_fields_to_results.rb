67bb3c2dcef600cfffa5babcbae2bbaed16bbff1
hasini <hasini.psk@gmail.com>
2011-07-23 19:45:37 +0530

    removed unnecessary migration

--- db/migrate/20110716142214_add_fields_to_results.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToResults < ActiveRecord::Migration
-  def self.up
-#    add_column :results, :department_description, :text
-  end
-
-  def self.down
-    remove_column :results, :department_description
-  end
-end
